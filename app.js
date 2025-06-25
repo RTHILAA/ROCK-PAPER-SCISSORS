5998331186f86a4956356bb94fc28a947b50fc1c
ANASS EL HARAZI <anaswins35@gmail.com>
2025-06-25 18:22:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,40 @@ let cImg = cChoice.previousElementSibling;
 
 let weapons = document.querySelectorAll(".img");
 let result = document.querySelector(".result");
+let gameContainer = document.querySelector(".game-container");
+
+let playerScore = 0;
+let computerScore = 0;
+
+let playerScoreSpan = document.getElementById("player-score");
+let computerScoreSpan = document.getElementById("computer-score");
+
+function startCountdown(callback) {
+  let count = 3;
+  result.innerText = count;
+
+  let interval = setInterval(() => {
+    count--;
+    if (count > 0) {
+      result.innerText = count;
+    } else {
+      clearInterval(interval);
+      result.innerText = ""; 
+      callback(); 
+    }
+  }, 1000);
+}
+
+function shakeElements(callback) {
+  pImg.classList.add('shake');
+  cImg.classList.add('shake');
+
+  setTimeout(() => {
+    pImg.classList.remove('shake');
+    cImg.classList.remove('shake');
+    if (callback) callback();
+  }, 500);
+}
 
 function randomNumber() {
   return Math.floor(Math.random() * 3);
@@ -16,51 +50,80 @@ function Computer() {
   let ele = choices[randomNumber()];
   cImg.src = `images/${ele}.png`;
   cImg.id = ele;
-  cChoice.innerHTML = ele;
+  cChoice.innerHTML = ele.toUpperCase();
 }
 
 weapons.forEach((weapon) => {
   weapon.addEventListener("click", () => {
-    let item = weapon.childNodes[1];
-    pImg.src = item.src;
-    pImg.id = weapon.id;
-    pChoice.innerHTML = weapon.id;
+    if (gameContainer.classList.contains('disabled')) return;
 
-    Computer();
-    check(cImg.id, pImg.id);
+    gameContainer.classList.add('disabled');
+
+    let playerChoiceId = weapon.id;
+    let playerChoiceSrc = weapon.querySelector("img").src;
+
+    weapon.classList.add('clicked');
+    setTimeout(() => weapon.classList.remove('clicked'), 200);
+
+    startCountdown(() => {
+      pImg.src = playerChoiceSrc;
+      pImg.id = playerChoiceId;
+      pChoice.innerHTML = playerChoiceId.toUpperCase();
+
+      Computer();
+
+      shakeElements(() => {
+        check(cImg.id, pImg.id);
+      });
+    });
   });
 });
 
-function check(a, b) {
+function check(computerChoice, playerChoice) {
+  pImg.classList.remove("winner", "loser", "draw");
+  cImg.classList.remove("winner", "loser", "draw");
+  result.classList.remove('show-result');
+
   let msg;
-  if (a == b) {
-    msg = "DRAW";
-  }
-  if (a == "rock" && b == "paper") {
-    msg = "You Win!";
-  } else if (a == "rock" && b == "scissors") {
-    msg = "You Lose!";
-  }
 
-  if (a == "paper" && b == "scissors") {
-    msg = "You Win!";
-  } else if (a == "paper" && b == "rock") {
-    msg = "You Lose!";
-  }
-
-  if (a == "scissors" && b == "rock") {
-    msg = "You Win!";
-  } else if (a == "scissors" && b == "paper") {
-    msg = "You Lose!";
+  if (computerChoice === playerChoice) {
+    msg = "DRAW!";
+  } else if (computerChoice === "rock" && playerChoice === "paper") {
+    msg = "YOU WIN!";
+    playerScore++;
+  } else if (computerChoice === "rock" && playerChoice === "scissors") {
+    msg = "YOU LOSE!";
+    computerScore++;
+  } else if (computerChoice === "paper" && playerChoice === "scissors") {
+    msg = "YOU WIN!";
+    playerScore++;
+  } else if (computerChoice === "paper" && playerChoice === "rock") {
+    msg = "YOU LOSE!";
+    computerScore++;
+  } else if (computerChoice === "scissors" && playerChoice === "rock") {
+    msg = "YOU WIN!";
+    playerScore++;
+  } else if (computerChoice === "scissors" && playerChoice === "paper") {
+    msg = "YOU LOSE!";
+    computerScore++;
   }
 
   result.innerHTML = msg;
-  document.body.style.pointerEvents = "none";
+  result.classList.add('show-result');
 
-  setInterval(() => {
-    result.innerHTML += ".";
-  }, 700);
-  setTimeout(() => {
-    location.reload();
-  }, 2200);
+  playerScoreSpan.textContent = `Player : ${playerScore}`;
+  computerScoreSpan.textContent = `Computer : ${computerScore}`;
+
+  if (msg === "YOU WIN!") {
+    pImg.classList.add("winner");
+    cImg.classList.add("loser");
+  } else if (msg === "YOU LOSE!") {
+    pImg.classList.add("loser");
+    cImg.classList.add("winner");
+  } else {
+    pImg.classList.add("draw");
+    cImg.classList.add("draw");
+  }
+
+  gameContainer.classList.remove('disabled');
 }
